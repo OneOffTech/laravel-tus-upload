42c072f313272187bb8d1e941a07471f30b55ed2
Alessio <alessio.vertemati@gmail.com>
2020-01-03 15:21:00 +0100

    Separate common js module and browser iife

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,29 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import {eslint} from 'rollup-plugin-eslint';
+import {terser} from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
 	{
 		input: 'assets/js/tusuploader.js',
-		output: {
-			file: pkg.main,
-			format: 'iife',
-			name: 'TusUploader'
-		},
+		output: [
+			{
+				file: pkg.browser,
+				format: 'iife',
+				name: 'TusUploader'
+			},
+			{
+				file: pkg.main,
+				format: 'cjs',
+			}
+		],
 		plugins: [
 			resolve({browser: true}),
 			eslint(),
-            commonjs(),
-            filesize()
+			commonjs(),
+            filesize(),
+			terser(),
 		]
 	}
 ];
